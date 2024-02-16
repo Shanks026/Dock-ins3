109459a41734108d1cb767b7cd378568923a7afe
Shanks026 <159107037+Shanks026@users.noreply.github.com>
2024-02-16 15:02:42 +0530

    Add files via upload

---
+++ src/main/java/com/Mave_batch105/Display.java
@@ -0,0 +1,17 @@
+package com.Mave_batch105;
+
+public class Display {
+	public void message(String str)
+	{
+		System.out.println("Is the " +str+ " Deployed ? ");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Display D = new Display();
+		D.message("file");
+		System.out.println("I achieved comedy");
+	}
+
+}
